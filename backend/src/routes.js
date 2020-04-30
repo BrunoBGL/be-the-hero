9100a0f100d49a43626982a9b7e24e2db9b70d3d
Bruno Gustavo <bruno2011gustavo@hotmail.om>
2020-04-30 19:03:02 -0300

    feat: adicionado validações na API

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,18 +4,26 @@ const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
+const {
+  createOngValidator,
+  createIncidentValidator,
+  indexIncidentValidator,
+  deleteIncidentValidator,
+  indexProfileValidator,
+  createSessionValidator
+} = require('./validators/validators');
 
 const routes = express.Router();
 
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', createOngValidator, OngController.create);
 routes.get('/ongs', OngController.index);
 
-routes.post('/incidents', IncidentController.create);
-routes.get('/incidents', IncidentController.index);
-routes.delete('/incidents/:id', IncidentController.delet);
+routes.post('/incidents', createIncidentValidator, IncidentController.create);
+routes.get('/incidents', indexIncidentValidator, IncidentController.index);
+routes.delete('/incidents/:id', deleteIncidentValidator, IncidentController.delet);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', indexProfileValidator, ProfileController.index);
 
-routes.post('/session', SessionController.create);
+routes.post('/session', createSessionValidator, SessionController.create);
 
 module.exports = routes;
